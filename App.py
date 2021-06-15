3360b8a68446ef549ce2e49c5b62cba4a6789415
Jose Gomez <jorago75@gmail.com>
2021-06-15 19:25:34 -0400

    Agregar mensajes flash

--- App.py
+++ App.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 from openpyxl import load_workbook
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+# Mysql Connection
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -12,18 +13,20 @@ app.config['MYSQL_DB'] = 'bd_cargadatos'
 Bootstrap(app)
 mysql = MySQL(app)
 
+# Settings
+app.secret_key= 'mysecretkey'
+
 @app.route('/')
 def Index():    
     return render_template('index.html', listado = obtenerDatos())
 
+@app.errorhandler(404)
+def page_not_found(error):
+ return render_template("404.html"), 404
+
 @app.route('/cargar', methods=['POST'])
 def Cargar():
     if request.method == 'POST':   
-        # cursor =  mysql.connection.cursor()
-        # cursor.execute("select * from datos")
-        # datos = cursor.fetchall()
-
-        # datos = obtenerDatos()
         #carga de archivo
         nombre_archivo = request.form['archivo']
         wb = load_workbook(nombre_archivo)
@@ -33,15 +36,11 @@ def Cargar():
         for sheet in sheets:
             #Hoja activa
             sheetActive = wb[sheet]
-            if sheet in wb.sheetnames and wb[sheet].max_column > 0 and wb[sheet].max_row > 0:                
+            if sheet in wb.sheetnames and wb[sheet].max_column == 5 and wb[sheet].max_row > 0:                
                 #Obtener la cantidad de Columnas
                 cols = wb[sheet].max_column
                 #Obtener la cantidad de Filas
                 rows = wb[sheet].max_row
-                # rows = 10
-                print('La hoja tiene columnas',cols)
-                print('La hoja tiene filas',rows)
-
                 #Recorrer cada fila y obtener los datos de cada column
                 for row in range(2, rows+1):
                     nombre= sheetActive.cell(row,1).value
@@ -49,14 +48,15 @@ def Cargar():
                     nacionalidad= sheetActive.cell(row,3).value
                     fechaContrato = sheetActive.cell(row,4).value
                     sexo = sheetActive.cell(row,5).value
-
-                    cursor = mysql.connection.cursor()
+                    # Insertar en la base de datos cada registro
+                    cursor = mysql.connection.cursor()                    
                     cursor.execute('INSERT INTO datos(nombre, apellido, nacionalidad, fechaContrato, sexo) values(%s,%s,%s,%s,%s)', (nombre, apellido, nacionalidad, fechaContrato, sexo))
                     mysql.connection.commit()
-                print('Carga Exitosa')
+                flash('La carga del archivo se ha ejecutado correctamente')
+
             else:
-                print('No existen datos para cargar')
-    return redirect(url_for('Index'))
+                flash('Error al intentar cargar el archivo, está mal formado o no contiene información')
+        return redirect(url_for('Index'))
 
 def obtenerDatos():
     cursor =  mysql.connection.cursor()
