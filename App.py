95c75959426e71b0ab2a0f59568f79f27616c864
Jose Gomez <jorago75@gmail.com>
2021-06-15 15:47:57 -0400

    Agregar funcion para la consulta de datos Modificar el template

--- App.py
+++ App.py
@@ -13,19 +13,17 @@ Bootstrap(app)
 mysql = MySQL(app)
 
 @app.route('/')
-def Index():
-    cursor =  mysql.connection.cursor()
-    cursor.execute("SELECT * FROM datos")
-    data = cursor.fetchall()
-    return render_template('index.html', listado = data)
+def Index():    
+    return render_template('index.html', listado = obtenerDatos())
 
 @app.route('/cargar', methods=['POST'])
 def Cargar():
     if request.method == 'POST':   
-        cursor =  mysql.connection.cursor()
-        cursor.execute("select * from datos")
-        datos = cursor.fetchall()
-        print('Desde la BD: ', datos)
+        # cursor =  mysql.connection.cursor()
+        # cursor.execute("select * from datos")
+        # datos = cursor.fetchall()
+
+        # datos = obtenerDatos()
         #carga de archivo
         nombre_archivo = request.form['archivo']
         wb = load_workbook(nombre_archivo)
@@ -35,19 +33,17 @@ def Cargar():
         for sheet in sheets:
             #Hoja activa
             sheetActive = wb[sheet]
-            if sheet in wb.sheetnames:
+            if sheet in wb.sheetnames and wb[sheet].max_column > 0 and wb[sheet].max_row > 0:                
                 #Obtener la cantidad de Columnas
                 cols = wb[sheet].max_column
                 #Obtener la cantidad de Filas
-                # rows = wb[sheet].max_row
-                rows = 10
-                # print('La hoja tiene columnas',cols)
-                # print('La hoja tiene filas',rows)
+                rows = wb[sheet].max_row
+                # rows = 10
+                print('La hoja tiene columnas',cols)
+                print('La hoja tiene filas',rows)
 
-                # datos = []
-                #Recorrer cada fila y obtener los datos de cada column                    registro[row] = sheetActive.cell(row,col).value
+                #Recorrer cada fila y obtener los datos de cada column
                 for row in range(2, rows+1):
-                    registro = []
                     nombre= sheetActive.cell(row,1).value
                     apellido= sheetActive.cell(row,2).value
                     nacionalidad= sheetActive.cell(row,3).value
@@ -57,15 +53,16 @@ def Cargar():
                     cursor = mysql.connection.cursor()
                     cursor.execute('INSERT INTO datos(nombre, apellido, nacionalidad, fechaContrato, sexo) values(%s,%s,%s,%s,%s)', (nombre, apellido, nacionalidad, fechaContrato, sexo))
                     mysql.connection.commit()
-                    # for col in range(1, cols+1):
-                    #     print(sheetActive.cell(row,col).value)
-                    #     registro = sheetActive.cell(row,col).value
-                    #     print('Datos de la fila: ',str(row))
-                    #     print('registro a guardar: ',registro)
-                    
                 print('Carga Exitosa')
-    # return 'Terminado...'
+            else:
+                print('No existen datos para cargar')
     return redirect(url_for('Index'))
+
+def obtenerDatos():
+    cursor =  mysql.connection.cursor()
+    cursor.execute("SELECT * FROM datos")
+    data = cursor.fetchall()
+    return data
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
